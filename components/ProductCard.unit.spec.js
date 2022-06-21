03dda33ba07ebd1cfae7a0b6275159d6911f8ecd
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-06-20 21:11:38 -0300

    feat: Create productCard

--- components/ProductCard.unit.spec.js
+++ components/ProductCard.unit.spec.js
@@ -1,14 +1,50 @@
 import { mount } from '@vue/test-utils';
 import ProductCard from '@/components/ProductCard';
+import { makeServer } from '@/miragejs/server';
 
 describe('ProductCard - unit', () => {
-  it('Should mount the component', () => {
-    const wrapper = mount(ProductCard, {
+  let server;
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' });
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
+  const mountProductCard = () => {
+    return mount(ProductCard, {
       propsData: {
-        product: {},
+        product: server.create('product', {
+          title: 'Relógio bonito',
+          price: '23.00',
+          image: 'http://placeimg.com/640/640/abstract?11788',
+        }),
       },
     });
+  };
+
+  it('Should match snapshot', () => {
+    const wrapper = mountProductCard();
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('Should mount the component', () => {
+    const wrapper = mountProductCard();
 
     expect(wrapper.vm).toBeDefined();
+    expect(wrapper.text()).toContain('Relógio bonito');
+    expect(wrapper.text()).toContain('$23.00');
+  });
+
+  it('Should emit the event addToCart with product object when button gets clicked', async () => {
+    const wrapper = mountProductCard();
+
+    await wrapper.find('button').trigger('click');
+
+    expect(wrapper.emitted().addToCart).toBeTruthy();
+    expect(wrapper.emitted().addToCart.length).toBe(1);
+    // expect(wrapper.emitted().addToCard[1]).toEqual([123]);
   });
 });
