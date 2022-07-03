c678f1e3fe6fcf6bc5853b2918856d3326a5b96f
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-07-03 16:57:56 -0300

    feat: Parser item in cart

---
+++ components/CartItem.unit.spec.js
@@ -0,0 +1,46 @@
+import { mount } from '@vue/test-utils';
+import CartItem from '@/components/CartItem';
+import { makeServer } from '@/miragejs/server';
+
+describe('CartItem', () => {
+  let server;
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' });
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
+  const mountCartItem = () => {
+    const product = server.create('product', {
+      title: 'Lindo relogio',
+      price: '22.33',
+    });
+    const wrapper = mount(CartItem, {
+      propsData: {
+        product,
+      },
+    });
+
+    return { wrapper, product };
+  };
+
+  it('Should mount the component', () => {
+    const { wrapper } = mountCartItem();
+
+    expect(wrapper.vm).toBeDefined();
+  });
+
+  it('Should display product info', () => {
+    const {
+      wrapper,
+      product: { title, price },
+    } = mountCartItem();
+
+    const content = wrapper.text();
+
+    expect(content).toContain(title);
+    expect(content).toContain(price);
+  });
+});
