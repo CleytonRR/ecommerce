311caece6983708a0a972dce786e18f4235e62aa
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-07-11 23:54:41 -0300

    feat: Added button to clear cart

--- components/Cart.unit.spec.js
+++ components/Cart.unit.spec.js
@@ -76,16 +76,28 @@ describe('Cart', () => {
   });
 
   it('Should display 2 instances of CartItem when products are provided', () => {
-    const products = server.createList('product', 2);
-    const wrapper = mount(Cart, {
-      propsData: {
-        products,
-      },
-    });
+    const { wrapper } = mountCart();
 
     const items = wrapper.findAllComponents(CartItem);
 
     expect(items).toHaveLength(2);
     expect(wrapper.text()).not.toContain('Cart is empty');
+  });
+
+  it('Should display a button to clear cart', () => {
+    const { wrapper } = mountCart();
+    const button = wrapper.find('[data-testid="clear-cart-button"]');
+
+    expect(button.exists()).toBe(true);
+  });
+
+  it('Should call cart manager removeProduct() when button gets clicked', async () => {
+    const { wrapper, cartManager } = mountCart();
+    const button = wrapper.find('[data-testid="clear-cart-button"]');
+    expect(button.exists()).toBe(true);
+    const spy = jest.spyOn(cartManager, 'clearProducts');
+
+    await button.trigger('click');
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 });
