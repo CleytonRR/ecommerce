c9ef15887a4ace27cf52516f756ef8c8bc03e150
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-07-14 00:01:38 -0300

    test: Created first e2e test

--- cypress/e2e/Store/store.cy.js
+++ cypress/e2e/Store/store.cy.js
@@ -1,5 +1,8 @@
 context('Store', () => {
   it('Should display the store', () => {
     cy.visit('http://localhost:3000');
+
+    cy.get('body').contains('Brand');
+    cy.get('body').contains('Wrist Watch');
   });
 });
