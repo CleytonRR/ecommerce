59650ae905dea1796f10465da9196b4ec9a8d00b
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-07-15 22:37:04 -0300

    test: Ensure get product by search

--- cypress/e2e/Store/store.cy.js
+++ cypress/e2e/Store/store.cy.js
@@ -21,12 +21,27 @@ context('Store', () => {
   });
 
   context('Store - Search for products', () => {
-    it.only('Should type in the search field', () => {
+    it('Should type in the search field', () => {
       cy.visit('http://localhost:3000');
 
       cy.get('input[type="search"]')
         .type('Some text here')
         .should('have.value', 'Some text here');
+    });
+
+    it('Should type in the search field', () => {
+      server.create('product', {
+        title: 'Relógio bonito',
+      });
+      server.create('product', 10);
+
+      cy.visit('http://localhost:3000');
+
+      cy.get('input[type="search"]').type('Relógio bonito');
+
+      cy.get('[data-testid="search-form"]').submit();
+
+      cy.get('[data-testid="product-card"]').should('have.length', 1);
     });
   });
 });
